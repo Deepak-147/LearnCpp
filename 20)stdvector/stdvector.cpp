e8cdef27447b161771b35cd78fa1021bacb0d868
Deepak Laxkar <deepaklaxkar11@gmail.com>
2018-07-30 16:14:40 +0530

    Commit on 30-07-2018@16:16PM

--- 20)stdvector/stdvector.cpp
+++ 20)stdvector/stdvector.cpp
@@ -2,10 +2,12 @@
 Introduced in C++03, std::vector provides dynamic array functionality that handles its own memory management.
 This means you can create arrays that have their length set at runtime, without having to explicitly allocate
 and deallocate memory using new and delete.
+std::vector can be used as a dynamic array that both remembers its length and can be dynamically resized as required.
 */
 
 #include<iostream>
 #include<vector>
+#include<array>
 int main(void) {
     // no need to specify length at initialization
     std::vector<int> array1;//Takes 0 by default
@@ -34,6 +36,35 @@ int main(void) {
         std::cout << element << ' ';
 
     std::cout << "\n";
+
+
+    /*Use of length and capacity. */
+    std::array<int, 10> array4 { 1, 2, 3, 4, 5 };
+
+    /*Or use this:
+    int *array4 = new int[10] { 1, 2, 3, 4, 5 };
+
+    We would say that this array has a length of 10, even though we are only using 5 of the elements that we allocated.
+    Unlike a built-in array or a std::array, which only remembers its length, std::vector contains two separate attributes: length and capacity.
+    In the context of a std::vector, length is how many elements are being used in the array, whereas capacity is how many elements were allocated in memory.
+    */
+    std::cout << "The length is: " << array4.size() << '\n';
+    /*
+    std::array does not have capacity member. So this will not work.
+    std::cout << "The capacity is: " << array4.capacity() << '\n';
+    */
+
+
+    std::vector<int> array5 {0, 1, 2, 3, 4};
+    array5 = {0, 1, 2, 3, 4};
+
+    std::cout << "The length is: " << array5.size() << '\n';
+    std::cout << "The capacity is: " << array5.capacity() << '\n';
+
+    array5 = {0, 1, 2};
+
+    std::cout << "The length is: " << array5.size() << '\n';
+    std::cout << "The capacity is: " << array5.capacity() << '\n';
 
     return 0;
 }
